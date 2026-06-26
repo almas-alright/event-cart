fc12c287e4922d341dfa61d6c68bc2061d440dfc
almas <almas@almass-MacBook-Pro.local>
2026-06-26 13:02:00 +0600

    feat(events): add nats publisher

--- src/eventcart/modules/events/__init__.py
+++ src/eventcart/modules/events/__init__.py
@@ -1,7 +1,15 @@
 """Event and outbox module."""
 
 from eventcart.modules.events.models import OutboxEvent, OutboxEventStatus
+from eventcart.modules.events.publisher import NatsEventPublisher, subject_for_event
 from eventcart.modules.events.repository import OutboxRepository
 from eventcart.modules.events.schemas import EventEnvelope
 
-__all__ = ["EventEnvelope", "OutboxEvent", "OutboxEventStatus", "OutboxRepository"]
+__all__ = [
+    "EventEnvelope",
+    "NatsEventPublisher",
+    "OutboxEvent",
+    "OutboxEventStatus",
+    "OutboxRepository",
+    "subject_for_event",
+]
